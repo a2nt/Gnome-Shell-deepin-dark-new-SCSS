15e8cfc0ff58f8edb7834dde7c123296b7223f17
Tony Air <tony@thewebdevelopmentagency.com>
2019-10-14 06:58:49 +0700

    Global variables + Color system

--- parse-sass.sh
+++ parse-sass.sh
@@ -7,7 +7,16 @@ fi
 
 SASSC_OPT="-M -t compact"
 
-echo Generating the css...
+echo Generating the gnome-shell css ...
+
+sassc $SASSC_OPT ./gnome-shell/src/gnome-shell.scss ./gnome-shell/gnome-shell.css
+
+echo Generating the gtk-3.0 css...
 
 sassc $SASSC_OPT ./gtk-3.0/src/gtk.scss ./gtk-3.0/gtk-light.css
 sassc $SASSC_OPT ./gtk-3.0/src/gtk-dark.scss ./gtk-3.0/gtk.css
+
+echo Generating cinnamon css ...
+
+sassc $SASSC_OPT ./cinnamon/src/cinnamon.scss ./cinnamon/cinnamon.css
+
